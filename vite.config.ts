039a3b363668e1565549402edbd265c801bb8a45
Anna Prutnik <prutnik.anna@gmail.com>
2023-09-29 12:15:27 +0300

    fix: remove base from vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kavaleridze-admin/ ',
   preview: {
     host: true,
     port: 5174,
