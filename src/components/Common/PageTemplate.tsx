5bfdd48e9468d1344598c4bb6524b180f843cc22
Yevhen <onufrii.socnet@gmail.com>
2023-10-03 18:51:38 +0300

    add: tabs to settings page

--- src/components/Common/PageTemplate.tsx
+++ src/components/Common/PageTemplate.tsx
@@ -1,37 +1,22 @@
-import { FC, PropsWithChildren } from 'react';
-import { Container, Divider, Typography, Stack } from '@mui/material';
+import { Container, Divider, Stack, Typography } from '@mui/material'
+import { FC, PropsWithChildren } from 'react'
 
 interface PageTemplateProps {
-  title: string;
+  title: string
 }
 
-const PageTemplate: FC<PropsWithChildren<PageTemplateProps>> = ({
-  title,
-  children,
-}) => {
+const PageTemplate: FC<PropsWithChildren<PageTemplateProps>> = ({ title, children }) => {
   return (
     <Stack width="100%">
-      <Container
-        sx={{
-          pt: { xs: 3, md: 7, lg: '68px' },
-          pb: { xs: 3, md: 3, lg: 2 },
-        }}
-      >
+      <Container sx={{ pt: { xs: 3, md: 7, lg: '68px' }, pb: { xs: 3, md: 3, lg: 2 } }}>
         <Typography variant="h3" component="h1">
           {title}
         </Typography>
       </Container>
       <Divider flexItem />
-      <Container
-        sx={{
-          pt: { xs: 4, md: 4, lg: 5 },
-          pb: { xs: '60px', md: 10, lg: 15 },
-        }}
-      >
-        {children}
-      </Container>
+      {children}
     </Stack>
-  );
-};
+  )
+}
 
-export default PageTemplate;
+export default PageTemplate
--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,7 +1,41 @@
-import PageTemplate from '../Common/PageTemplate';
+import { Box, BoxProps, Divider, Tab, Tabs } from '@mui/material'
+import { FC, ReactNode, SyntheticEvent, useState } from 'react'
+import PageTemplate from '../Common/PageTemplate'
+import SvgSpriteIcon from '../Common/SvgSprite'
 
-const Settings = () => {
-  return <PageTemplate title="Налаштування">Settings</PageTemplate>;
-};
+interface TabPanelProps extends BoxProps {
+  children?: ReactNode
+  index: number
+  value: number
+}
+const TabPanel: FC<TabPanelProps> = ({ index, value, children, ...props }) => {
+  return <Box {...props}>{value === index && <Box>{children}</Box>}</Box>
+}
 
-export default Settings;
+const Settings: FC = () => {
+  const [value, setValue] = useState(0)
+
+  const handleChange = (event: SyntheticEvent, newValue: number) => {
+    setValue(newValue)
+  }
+
+  return (
+    <PageTemplate title="Налаштування">
+      <Box>
+        <Tabs value={value} onChange={handleChange}>
+          <Tab sx={{ padding: 0 }} label="Змінити пароль" />
+          <Tab sx={{ padding: 0 }} label="Змінити логін" />
+        </Tabs>
+        <Divider />
+        <TabPanel index={0} value={value}>
+          <SvgSpriteIcon iconId="eye-close" />
+        </TabPanel>
+        <TabPanel index={1} value={value}>
+          <SvgSpriteIcon iconId="eye-open" />
+        </TabPanel>
+      </Box>
+    </PageTemplate>
+  )
+}
+
+export default Settings
