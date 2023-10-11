ac5294907f90976296efee325565574f4f9f294e
Anna Prutnik <prutnik.anna@gmail.com>
2023-10-11 11:56:19 +0300

    fix: dockerfile, add config for nginx, fix container margin

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,11 @@
-import { IEventValues, IContactInfo } from "@/types/events";
-import axios from "axios";
+import { IEventValues, IContactInfo } from '@/types/events';
+import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_SERVER_URL;
 // console.log(BASE_URL);
 
 export const instance = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true,
 });
 
 interface LoginResponse {
@@ -15,25 +14,25 @@ interface LoginResponse {
 
 export const login = (username: string, password: string) => {
   return instance.post<LoginResponse>(
-    "/admin/login",
+    '/admin/login',
     {},
     { auth: { username, password } }
   );
 };
 
 export const logout = () => {
-  return instance.post("/admin/logout");
+  return instance.post('/admin/logout');
 };
 
 export const getContactInfo = () => {
-  console.log("BASE-URL from environment", BASE_URL);
-  console.log("axios base url", instance.getUri());
+  console.log('BASE-URL from environment', BASE_URL);
+  console.log('axios base url', instance.getUri());
 
-  return instance.get("/museum_data");
+  return instance.get('/museum_data');
 };
 
 export const updateContactInfo = (data: IContactInfo) => {
-  return instance.put("/admin/museum_data", data);
+  return instance.put('/admin/museum_data', data);
 };
 
 interface AddImageResponse {
@@ -41,14 +40,14 @@ interface AddImageResponse {
 }
 
 export const addNewImage = (file: File | Blob) => {
-  const headers = { "Content-Type": "multipart/form-data" };
+  const headers = { 'Content-Type': 'multipart/form-data' };
   const formData = new FormData();
-  formData.append("file", file);
-  return instance.post<AddImageResponse>("/admin/images", formData, {
+  formData.append('file', file);
+  return instance.post<AddImageResponse>('/admin/images', formData, {
     headers,
   });
 };
 
 export const addEvent = (data: IEventValues) => {
-  return instance.post("/admin/events", data);
+  return instance.post('/admin/events', data);
 };
